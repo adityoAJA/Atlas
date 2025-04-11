ac3e3a9c4538a670586359aeee2c681342cd5ee4
adityoAJA <168532434+adityoAJA@users.noreply.github.com>
2025-04-11 12:54:21 +0700

    Update Pendahuluan.py

--- Pendahuluan.py
+++ Pendahuluan.py
@@ -11,7 +11,7 @@ st.set_page_config(
     )
 
 # Load custom CSS file
-with open('style.css') as f:
+with open('https://raw.githubusercontent.com/adityoAJA/Atlas/main/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 ######################################################
